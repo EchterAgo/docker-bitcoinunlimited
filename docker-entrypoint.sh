6d93c3db0de9371414cb292a37d25905b7a1b457
Axel Gembe <derago@gmail.com>
2020-01-10 18:00:21 +0700

    Remove setting -datadir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,10 +9,4 @@ if [ $(echo "$1" | cut -c1) = "-" ]; then
   set -- bitcoind "$@"
 fi
 
-if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
-  chown -R $(id -u):$(id -g) "$BITCOIN_DATA"
-  echo "$0: setting data directory to $BITCOIN_DATA"
-  set -- "$@" -datadir="$BITCOIN_DATA"
-fi
-
 exec "$@"
